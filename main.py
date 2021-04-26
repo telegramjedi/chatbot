add16c337c9bb5c4966de0197f797460e512d4a0
JuanFSR <juanfrangel15@gmail.com>
2021-04-26 10:11:11 -0300

    Buidpack added

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+
+url = 'https://chatbotclone.herokuapp.com/webhooks/rest/webhook' ##change rasablog with your app name
+
+myobj = {
+"message": "hi",
+"sender": "Juan Rangel",
+}
+
+x = requests.post(url, json = myobj)
+print(x.text)
